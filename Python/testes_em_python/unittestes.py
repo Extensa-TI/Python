947fdbe40217f48b084e7079f58334204b0c8838
Extensa-TI <extensati@gmail.com>
2025-07-15 10:23:06 -0300

    Primeiro commit

---
+++ Python/testes_em_python/unittestes.py
@@ -0,0 +1,21 @@
+"""
+assertEqual(a,b)        -> a == b
+assertNotEqual(a,b)     -> a != b
+assertTrue(x)           -> x é true
+assertFalse(x)          -> x é false
+assertIs(a,b)           -> a é b
+assertIsNot(a,b)        -> a não é b
+assertIfNone(x)         -> x é None
+assertIfNotNone(x)      -> x não é None
+assertIn(a,b)           -> a está em b
+assertNotIn(a,b)        -> a não está em b
+assertIsInstance(a,b)   -> se a é instância de b
+assertNotIsInstance(a,b)-> se a não é instância de b
+
+Por convenção, todos os testes em um test case devem ter seu nome iniciado com test_
+
+Para executar os testes no nodo normal -> python nome_do_modulo.py
+Para executar os testes no modo verbose -> python nome_do_modulo.py -v
+
+Podemos acrescentar e é recomendadod docstrtings nos testes.
+"""
